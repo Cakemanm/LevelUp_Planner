12ae935809d5abda5643de52430ffafd5e84e8c9
Cakemanm <jam82905@gmail.com>
2026-03-18 17:19:56 -0400

    username textbox

--- app/src/main/java/com/example/levelup_planner/MainActivity.kt
+++ app/src/main/java/com/example/levelup_planner/MainActivity.kt
@@ -4,28 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.levelup_planner.ui.theme.LevelUp_PlannerTheme
 
+//Added Imports
+import androidx.compose.runtime.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.*
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LevelUp_PlannerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+//            LevelUp_PlannerTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+            MyTextBox()
         }
     }
 }
@@ -36,6 +44,42 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+//Username Textbox Test
+@Composable
+fun MyTextBox() {
+    //hold typing name in variable
+    var typingName by remember { mutableStateOf("") }
+
+    //hold final inputted name
+    var savedName by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        //input text field
+        TextField(
+            value = typingName, //what is currently shown
+            onValueChange = { newText -> typingName = newText }, //update
+            label = { Text("Enter your name") } //floating label
+        )
+
+        //button to submit name
+        Button(onClick = {
+            savedName = typingName
+        }) {
+            Text("Sumbit")
+        }
+
+        Text(
+            text = "Username: $savedName"
+        )
+    }
+
+
 }
 
 @Preview(showBackground = true)
